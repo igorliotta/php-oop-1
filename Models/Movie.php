666d58becf33c12a49d013c7a13bbe9f730e3725
igorliotta <liottaigor@gmail.com>
2023-11-29 15:21:24 +0100

    Added: in Models folder Serie.php and Movie.php

--- Models/Movie.php
+++ Models/Movie.php
@@ -1,5 +1,36 @@
 <?php
 
+// Recupero il file Production.php tramite un require per averlo disponibile nel componente
+require_once __DIR__ . ('/Production.php');
 
+// La classe Movie estenderà la classe Production e aggiungerà 2 proprietà: $profit (incassi),  e $duration (con relativi metodi get / set)
+class Movie extends Production {
+    protected $profit;
+    protected $duration;
+
+    public function setProfit($profit) {
+        if (is_numeric($profit) && $profit > 0) {
+            $this->profit = $profit;
+        } else {
+            echo 'Il valore $profit non è valido';
+        }
+    }
+
+    public function getProfit() {
+        return $this->profit;
+    }
+
+    public function setDuration($duration) {
+        if (is_numeric($duration) && $duration > 0) {
+            $this->rating = $duration;
+        } else {
+            echo 'Il valore $duration non è valido';
+        }
+    }
+
+    public function getDuration() {
+        return $this->duration;
+    }
+}
 
 ?>
--- Models/Serie.php
+++ Models/Serie.php
@@ -1,5 +1,24 @@
 <?php
 
+// Recupero il file Production.php tramite un require per averlo disponibile nel componente
+require_once __DIR__ . ('/Production.php');
 
+// La classe Serie estenderà anch’essa Production aggiungendo la proprietà $season  (con relativi metodi get / set) ovvero il numero della stagione
+class Serie extends Production {
+    protected $season;
+
+    public function getSeason() {
+        return $this->season;
+    }
+
+    public function setSeason($season) {
+        if (is_numeric($season) && $season > 0) {
+            $this->season = $season;
+        } else {
+            echo 'Il valore $season non è valido';
+        }
+    }
+
+}
 
 ?>
